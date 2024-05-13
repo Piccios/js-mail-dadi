5bc8ac7a09af9a177158995138ac403546c7a1a3
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-13 17:16:47 +0200

    the button is active with js: when the input is filled and the putton is clicked, JS analizes the email written. when the email il correct: an allert wolcomes the user. when the email is not on the list: the user is not allowed

--- mail/Js/script.js
+++ mail/Js/script.js
@@ -1,2 +1,13 @@
 const usersMails = ["lorenzo.picchi@gmail.com", "marco.bianchi@gmail.com", "paolo.rossi@gmail.com", "martina.rossi@gmail.com" , "giulia.bianchi@gmail.com"];
 
+const login = document.getElementById("inputEmail");
+
+document.getElementById("btn").addEventListener("click", function(){
+    if (usersMails.includes(login.value)){
+        alert("Benvenuto, sei uno dei prescelti.");
+    }else{
+        alert("Non sei autorizzato");
+    }
+    
+});
+
